13a37904cd94a616becb1357b741652518f535cb
shazad_alam <shazad.alam.cse@ulab.edu.bd>
2024-09-04 07:06:40 +0100

    gcp info updated

--- createrelease.sh
+++ createrelease.sh
@@ -2,10 +2,10 @@
 echo "Building image for 1st release"
 echo
 ###
-skaffold build --default-repo=yourrepo --file-output=artifacts.json
+skaffold build --default-repo=us-central1-docker.pkg.dev/gcp-learn-430710/cicd-test --file-output=artifacts.json
 ###
 echo "Creating 1st release"
 echo
-gcloud deploy releases create first-release --delivery-pipeline cd-on-gcp-pipeline --region yourregion --build-artifacts artifacts.json
+gcloud deploy releases create first-release --delivery-pipeline cd-on-gcp-pipeline --region us-central1 --build-artifacts artifacts.json
 ###
 echo "Release creation completed, please promote and approve your release manually"
